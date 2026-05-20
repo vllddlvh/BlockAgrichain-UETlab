f380380527dceb39d270e0175b8e43dfc2bde7cc
PhuongBui69 <buihaiphupng6@gmail.com>
2026-05-20 16:01:31 +0700

    Update: Seed data

--- Blockagrichain-Backend/src/main/java/BlockchainAgridence/uet/configuration/SecurityConfig.java
+++ Blockagrichain-Backend/src/main/java/BlockchainAgridence/uet/configuration/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
     private final String[] PUBLIC_ENDPOINTS = {
             "/api/v1/organizations/register",
             "/api/v1/auth/login",
+            "/api/v1/auth/logout",
             "/api/v1/auth/refresh",
             "/api/v1/auth/introspect",
             "/api/v1/ipfs/upload"
